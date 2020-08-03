220e6ff21635c43dc9f83ca5cfa34a24d850f531
Md Nurul Amin <mohami@Mds-MacBook-Pro.lan>
2020-08-03 15:23:33 +0200

    intt project

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?units=m&access_key=9e6f1300fb462f8042bab2d6e95f1880&query=' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out.')
+        }
+    })
+}
+
+module.exports = forecast
